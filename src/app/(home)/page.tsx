48a46040cf30b02c03dce0309bb99e5c11557416
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-10-18 12:34:01 -0300

    :construction: feat: add computeProductTotalPrice

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,33 @@
-"use client";
-
-import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Categories from "./components/categories";
+import ProductList from "./components/product-list";
+import { prismaClient } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
-    <div className="p-5">
+    <div>
       <Image
         src="/banner-home-01.png"
         alt="até 55% de desconto só esse mês"
         width={0}
         height={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vw"
         priority
       />
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+      <div className="mt-5 px-5">
+        <ProductList products={deals} />
       </div>
     </div>
   );
