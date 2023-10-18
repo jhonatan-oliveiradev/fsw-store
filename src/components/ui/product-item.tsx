e1ba55f13c3462f6da0b391e5985c2dcb762a1a9
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-10-18 13:27:35 -0300

    :construction: feat: add ProductList and ProductItem components

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,14 +1,16 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
+import { Badge } from "./badge";
+import { ArrowDownIcon } from "lucide-react";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           alt={product.name}
@@ -18,11 +20,33 @@ const ProductItem = ({ product }: ProductItemProps) => {
           className="h-auto max-h-[70%] w-auto max-w-[80%]"
           style={{ objectFit: "contain" }}
         />
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDownIcon size={14} />
+            {product.discountPercentage}%
+          </Badge>
+        )}
       </div>
-      <div>
+      <div className="flex flex-col gap-1">
         <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="text-sm font-semibold">
+                R$ {product.totalPrice.toFixed(2).replace(".", ",")}
+              </p>
+              <p className="text-xs text-muted-foreground line-through">
+                R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2).replace(".", ",")}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
