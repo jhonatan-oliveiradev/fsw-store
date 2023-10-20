bb420608f7f2be909fba20b0dd5e51752d0e613d
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-10-20 12:30:19 -0300

    :construction: feat: add CartContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/ui/footer";
 import Container from "@/components/ui/container";
 
 import "./globals.css";
+import CartProvider from "@/providers/cart";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,9 +25,11 @@ export default function RootLayout({
       <body className={inter.className}>
         <Container>
           <AuthProvider>
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
+            <CartProvider>
+              <Header />
+              <main className="flex-1">{children}</main>
+              <Footer />
+            </CartProvider>
           </AuthProvider>
         </Container>
       </body>
