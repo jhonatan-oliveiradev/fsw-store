9551603e21b15d1e4c053ce380797fef00d1775b
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-10-21 15:22:36 -0300

    :wrench: fix: localStorage is not defined

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
-import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 
